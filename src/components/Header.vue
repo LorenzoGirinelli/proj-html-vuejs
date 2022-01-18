913f0f5cc951371a537c698d7a057c55b40e7666
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-01-18 18:11:32 +0100

    jumbotron

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,53 +1,58 @@
 <template>
-        <header class="container">
-        <!-- Logo -->
-        <div class="logo">
-            <img src="../assets/img/dark-logo.png" alt="logo">
-        </div>
-        <!-- Header Nav -->
-        <div>
-            <div class="nav">
-                <div class="nav-element">
-                    Home <i class="fas fa-chevron-down"></i>
+    <header>
+        <div class="head container">
+             <!-- Logo -->
+            <div class="logo">
+                <img src="../assets/img/dark-logo.png" alt="logo">
+            </div>
+             <!-- Header Nav -->
+            <div>
+                <div class="nav">
+                    <div class="nav-element">
+                          Home <i class="fas fa-chevron-down"></i>
+                    </div>
+                    <div class="nav-element">
+                        Pages <i class="fas fa-chevron-down"></i>
+                    </div>
+                    <div class="nav-element">
+                        Courses <i class="fas fa-chevron-down"></i>
+                    </div>
+                    <div class="nav-element">
+                        Features <i class="fas fa-chevron-down"></i>
+                    </div>
+                    <div class="nav-element">
+                        Bolg <i class="fas fa-chevron-down"></i>
+                    </div>
+                    <div class="nav-element">
+                        Shop <i class="fas fa-chevron-down"></i>
+                    </div>
                 </div>
-                <div class="nav-element">
-                    Pages <i class="fas fa-chevron-down"></i>
+            </div>
+            <div class="social-media">
+                <div class="social">
+                    <i class="fab fa-twitter"></i>
                 </div>
-                <div class="nav-element">
-                    Courses <i class="fas fa-chevron-down"></i>
+                <div class="social">
+                    <i class="fab fa-facebook-f"></i>
                 </div>
-                <div class="nav-element">
-                    Features <i class="fas fa-chevron-down"></i>
+                <div class="social">
+                    <i class="fab fa-instagram"></i>
                 </div>
-                <div class="nav-element">
-                    Bolg <i class="fas fa-chevron-down"></i>
-                </div>
-                <div class="nav-element">
-                    Shop <i class="fas fa-chevron-down"></i>
+                <div class="social">
+                    <i class="fab fa-linkedin"></i>
                 </div>
             </div>
         </div>
-        <div class="social-media">
-            <div class="social">
-                <i class="fab fa-twitter"></i>
-            </div>
-            <div class="social">
-                <i class="fab fa-facebook-f"></i>
-            </div>
-            <div class="social">
-                <i class="fab fa-instagram"></i>
-            </div>
-            <div class="social">
-                <i class="fab fa-linkedin"></i>
-            </div>
-        </div>
-        </header>
+    </header>
 </template>
 
 <style scoped lang="scss">
 @import '../style/general.scss';
-header{
-    font-family: Arial, Helvetica, sans-serif;
+// header{
+//         position: absolute;
+// }
+.head{
+    font-family: 'Playfair Display', serif;
     height: 105px;
     line-height: 100px;
     display: flex;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,45 @@
 <template>
     <main>
-        
+        <div class="jumbotron">
+            <div class="jumbo container">
+                <div class="jumbo-text">
+                    <h3>I'M WINGMAN</h3>
+                    <h2>Peace on Earth A Wondewrful Wish But No Way</h2>
+                    <p>Success isn't overnight. It's when everyday you get a little better then the day before. It all adds up.</p>
+                    <button class="green_btn">
+                        Get Started For Free <i class="fas fa-long-arrow-alt-right"></i>
+                    </button>
+                </div>
+                <img src="../assets/img/home-movation-hero-image.png" alt="">
+            </div>
+        </div>
     </main>
 </template>
+
+<style scoped lang="scss">
+@import '../style/general.scss';
+.jumbo{
+    background-image: url("../assets/img/background-pattern-wavify.png");
+    font-family: 'Playfair Display', serif;
+    display: flex;
+    align-items: center;
+    padding-top: 100px;
+    .jumbo-text{
+        padding-right: 300px;
+        h3{
+            font-weight: lighter;
+            font-size: 17px;
+        }
+        h2{
+            font-size: 55px;
+            line-height: 75px;
+            font-weight: lighter;
+        }
+        p{
+            line-height: 30px;
+            color: #333333;
+            padding-top: 20px;
+        }
+    }
+}
+</style>
